c74a43d1c33e6676e21deb7e03ab1143b47e8851
asterism1030 <rlaguswl1030@gmail.com>
2023-10-25 19:46:29 +0900

    Two way binding 구현

--- DataBinding/TwoWayBinding.xaml.cs
+++ DataBinding/TwoWayBinding.xaml.cs
@@ -19,9 +19,42 @@ namespace DataBinding
     /// </summary>
     public partial class TwoWayBinding : Window
     {
+        Person person = new Person { Name = "khj", Age = "30" };
+
         public TwoWayBinding()
         {
             InitializeComponent();
+
+            // 데이터 바인딩
+            this.DataContext = person;
         }
+
+        // 버튼 클릭 이벤트
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            string msg = person.Name + " " + person.Age;
+            txtBlock.Text = msg;
+        }
+
+    }
+
+    // 바인딩 시 get, set 해줄 클래스
+    public class Person
+    {
+        private string nameVal = "";
+        private string ageVal = "";
+
+        public string Name
+        {
+            get { return nameVal; }
+            set { nameVal = value; }
+        }
+
+        public string Age
+        {
+            get { return ageVal; }
+            set { ageVal = value; }
+        }
+
     }
 }
