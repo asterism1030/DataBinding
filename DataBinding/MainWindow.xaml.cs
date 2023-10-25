f428be3c7fece5672621ca0cad551fef8aae0d84
asterism1030 <rlaguswl1030@gmail.com>
2023-10-25 19:13:03 +0900

    초기 커밋.

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 데이터 바인딩
+            DataContext = Info.GetInfo();
+        }
+
+        public class Info
+        {
+            public string Name { get; set; }
+            public string Age { get; set; }
+
+            public static Info GetInfo()
+            {
+                Info info = new Info()
+                {
+                    Name = "khj",
+                    Age = "30's"
+                };
+                return info;
+            }
+        }
+
+
+
+    }
+}
